9efb3401b089892521997544b9eeed12d57cde32
aadideshpande <aadi.deshpande1@gmail.com>
2025-08-03 09:51:53 -0500

    set up rabbitmq as a message broker

--- services/price-service/app/main.py
+++ services/price-service/app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-import os
-import asyncio
-import random
+import os, asyncio, random, json
 from typing import Dict
+import aio_pika
 
 load_dotenv()
 app = FastAPI()
@@ -15,6 +14,10 @@ prices: Dict[str, float] = {
     "MSFT": 300.0,
 }
 
+RABBITMQ_URL = os.getenv("RABBITMQ_URL")
+QUEUE_NAME = os.getenv("RABBITMQ_QUEUE", "price.updates")
+rabbitmq_channel = None
+
 @app.get("/price/{symbol}")
 def get_price(symbol: str):
     price = prices.get(symbol.upper())
@@ -22,13 +25,42 @@ def get_price(symbol: str):
         return {"error": "Symbol not found"}
     return {"symbol": symbol.upper(), "price": round(price, 2)}
 
+async def publish_price_updates():
+    global rabbitmq_channel
+    retries = 5
+    for attempt in range(retries):
+        try:
+            connection = await aio_pika.connect_robust(RABBITMQ_URL)
+            channel = await connection.channel()
+            await channel.declare_queue(QUEUE_NAME, durable=True)
+            rabbitmq_channel = channel
+            print(f"Connected to RabbitMQ and declared queue '{QUEUE_NAME}'")
+            return
+        except Exception as e:
+            print(f"[Retry {attempt+1}/{retries}] RabbitMQ not ready. Retrying in 3s...")
+            await asyncio.sleep(3)
+
+    raise RuntimeError("RabbitMQ not available after multiple attempts.")
+
 async def simulate_price_updates():
     while True:
         for symbol in prices:
             drift = random.uniform(-1, 1)
             prices[symbol] += drift
+
+            # publish update
+            message = json.dumps({
+                "symbol": symbol,
+                "price": round(prices[symbol], 2)
+            })
+            if rabbitmq_channel:
+                await rabbitmq_channel.default_exchange.publish(
+                    aio_pika.Message(body=message.encode()),
+                    routing_key=QUEUE_NAME,
+                )
         await asyncio.sleep(2)
 
 @app.on_event("startup")
-async def start_simulation():
+async def startup_event():
+    await publish_price_updates()
     asyncio.create_task(simulate_price_updates())
